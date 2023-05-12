929f46bb3f4cc3bf1326a5ae8701a3e1ac9c4ffb
이승욱/모바일FO <lsa3163@gsretail.com>
2023-05-12 13:47:09 +0900

    feat: key 수정

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+        Authorization: `Bearer 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`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
