f7610e7567a4fa789a186e21b5d19f6a2b928911
이승욱/모바일FO <lsa3163@gsretail.com>
2023-05-12 11:09:33 +0900

    feat: gpt 처리

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
+  const [chargingStations, setChargingStations] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (location.latitude && location.longitude) {
+      // 위치 정보를 토대로 질문을 생성합니다.
+      const question = `
+      전기차 충전소 정보: 위도 ${location.latitude}, 경도 ${location.longitude} 근처에 있는 전기차 충전소는 어디인가요? 최신 정보는 필요없습니다. 또한 답변의 예시는 [A 충전소, B 충전소, C 충전소]로 알려줘.`;
+
+      gpt(question);
+    }
+  }, [location]);
+
+  async function gpt(qna) {
+    fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: "야!" }],
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(JSON.stringify(data, null, 2)));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>위치 정보</h1>
+      {location.latitude && location.longitude ? (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          위도: {location.latitude}, 경도: {location.longitude}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ) : (
+        <p>위치 정보를 가져오는 중...</p>
+      )}
+
+      <h1>전기차 충전소 정보</h1>
+      {chargingStations ? (
+        <p>{chargingStations}</p>
+      ) : (
+        <p>전기차 충전소 정보를 가져오는 중...</p>
+      )}
     </div>
   );
 }
